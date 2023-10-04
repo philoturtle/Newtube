600b03ad10191e241bc4418fd2aa2e26ee10e523
Maria <m.muszynska098@gmail.com>
2023-10-04 12:41:51 +0200

    I have added the ability to download input url and the --help

--- newtube.py
+++ newtube.py
@@ -1,5 +1,13 @@
+import argparse
 import pytube
-link = "https://www.youtube.com/watch?v=wwux9KiBMjE"
+
+parser = argparse.ArgumentParser(description= 'This program is downloading video from YouTube')
+parser.add_argument("-l","--link", help="downloading the video from input url")
+args = parser.parse_args()
+if args.link:
+	print("Downloading the video in progress")
+
+link = args.link
 yt = pytube.YouTube(link)
 stream = yt.streams.first()
 stream.download('/mnt/c/Users/mmusz/Downloads')
