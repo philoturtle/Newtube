85e91589bd6d05fcdd4b3a290d4de4219f7d91d0
Maria <m.muszynska098@gmail.com>
2023-10-04 12:53:29 +0200

    Adding regex checking. If input is correct

--- newtube.py
+++ newtube.py
@@ -1,13 +1,21 @@
 import argparse
 import pytube
+import re
+
 
 parser = argparse.ArgumentParser(description= 'This program is downloading video from YouTube')
 parser.add_argument("-l","--link", help="downloading the video from input url")
 args = parser.parse_args()
-if args.link:
-	print("Downloading the video in progress")
-
 link = args.link
+
+pattern = 'youtube.com'
+result = re.match(pattern, link)
+if result:
+	print('Downloading the video in  progress')
+else:
+	print('Error: Wrong url. Use --help')
+	exit(1)
+	
 yt = pytube.YouTube(link)
 stream = yt.streams.first()
 stream.download('/mnt/c/Users/mmusz/Downloads')
