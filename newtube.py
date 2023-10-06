a5383b709b31665ba349381d607cd8114a215463
Maria <m.muszynska098@gmail.com>
2023-10-06 11:15:12 +0200

    Class and function added

--- newtube.py
+++ newtube.py
@@ -1,22 +1,40 @@
 import argparse
 import pytube
 import re
+import typing
+
+# zrób klase
+# podziel na funkcje
+# jak używać if __name__ == "__main__"
 
 
-parser = argparse.ArgumentParser(description= 'This program is downloading video from YouTube')
-parser.add_argument("-l","--link", help="downloading the video from input url")
-args = parser.parse_args()
-link = args.link
+class YTFilm:
+    def __init__(self, link: str) -> str:
+        self.link = link
+        self.downloaded_film = pytube.YouTube(link)
 
-pattern = 'youtube.com'
-result = re.match(pattern, link)
-if result:
-	print('Downloading the video in  progress')
-else:
-	print('Error: Wrong url. Use --help')
-	exit(1)
-	
-yt = pytube.YouTube(link)
-stream = yt.streams.first()
-stream.download('/mnt/c/Users/mmusz/Downloads')
-print('Video downloaded')
+    def downloading(self) -> str:
+        stream = self.downloaded_film.streams.first()
+        stream.download("/mnt/c/Users/mmusz/Downloads")
+        print("Video downloaded")
+
+    def url_check(self) -> bool:
+        result = re.search(r"youtube\.com", self.link)
+        if result:
+            print("Downloading the video in  progress")
+            return True
+        else:
+            print("Error: Wrong url. Use --help", result)
+            return False
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="This program is downloading video from YouTube"
+    )
+    parser.add_argument("-l", "--link", help="downloading the video from input url")
+    args = parser.parse_args()
+    link = args.link
+    ytfilm = YTFilm(link)
+    if ytfilm.url_check():
+        ytfilm.downloading()
